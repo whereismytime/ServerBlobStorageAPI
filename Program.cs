e37fff5593ec9a7660566836ceb8c8a157713ebf
whereismytime <a.v.dyma@gmail.com>
2025-03-27 15:02:10 +0000

    War with swagger on AZURE

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blob Storage API v1");
-        c.RoutePrefix = "swagger"; // Теперь Swagger будет доступен по /swagger
+        c.RoutePrefix = string.Empty; // Swagger доступен прямо на корне сайта
     });
 }
 
